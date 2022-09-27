a4072eb39754be33a1aabc1ce82172e199149563
DyerSylvie <sylvierui1@gmail.com>
2022-09-27 13:01:43 -0400

    added template building page containers
    includes functionality to hide and show the feed / building pages depending on what is selected

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("defaultOpen").click();
 // filter out the posts 
 function showFeed(e, selection){
     // declare variables
-    var i, samplePost, filter;
+    var i, samplePost, filter, buildingPage;
 
     // gets elements with class="samplePost"
     samplePost = document.getElementsByClassName("samplePost");
@@ -13,13 +13,64 @@ function showFeed(e, selection){
         samplePost[i].style.display = "none";
     }
 
-    // get all elements with class="filter" and remove class "Active"
+    // get all feed filters (class="filter") and remove class "Active", change style
     filter = document.getElementsByClassName("filter");
     for (i = 0; i < filter.length; i++) {
         filter[i].className = filter[i].className.replace(" active", "");
+        filter[i].style = "color: grey";
     }
 
     // show current tab + add active label to button of tab
     document.getElementById(selection).style.display = "block";
     e.currentTarget.className += " active";
+    e.currentTarget.style = "color: black";
+
+     /* REMOVING BUILDING PAGE */
+    // get all building pages (class="buildingPage") and hide them 
+    buildingPage = document.getElementsByClassName("buildingPage");
+    for (x = 0; x < buildingPage.length; x++){
+        buildingPage[x].style.display = "none";
+    }
+
+    // get all building buttons  (class="buildingSelect") and remove style and active status
+    buildingSelect = document.getElementsByClassName("buildingSelect");
+    for (x = 0; x < buildingSelect.length; x++){
+        buildingSelect[x].className = buildingSelect[x].className.replace("active", "");
+    }
+}
+
+// show building hub
+function showBuilding(b, building){
+    // declare variables
+    var x, buildingPage, buildingSelect, alertFeed;
+
+    // get all building pages (class="buildingPage") and hide them 
+    buildingPage = document.getElementsByClassName("buildingPage");
+    for (x = 0; x < buildingPage.length; x++){
+        buildingPage[x].style.display = "none";
+    }
+
+    // get all building buttons  (class="buildingSelect") and remove style and active status
+    buildingSelect = document.getElementsByClassName("buildingSelect");
+    for (x = 0; x < buildingSelect.length; x++){
+        buildingSelect[x].className = buildingSelect[x].className.replace("active", "");
+    }
+
+    // show building page and add active label to button of selected building
+    document.getElementById(building).style.display = "block";
+    b.currentTarget.className += "active";
+
+    /* REMOVING FEED SECTION / STYLING */
+    // hide all the feed posts
+    alertFeed = document.getElementsByClassName("samplePost");
+    for (x = 0; x < alertFeed.length; x++){
+        alertFeed[x].style.display = "none";
+    }
+
+    // get all feed filters (class="filter") and remove class "Active", change style
+    filter = document.getElementsByClassName("filter");
+    for (i = 0; i < filter.length; i++) {
+        filter[i].className = filter[i].className.replace(" active", "");
+        filter[i].style = "color: grey";
+    }
 }
