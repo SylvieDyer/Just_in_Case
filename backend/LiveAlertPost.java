e4fccf3123ace8f33829e6c6ca27ab4fc97288fb
alp133 <102556053+alp133@users.noreply.github.com>
2022-10-18 12:46:40 -0400

    changed livealertpost categorization

--- backend/LiveAlertPost.java
+++ backend/LiveAlertPost.java
@@ -5,7 +5,7 @@ public class LiveAlertPost {
     /*class represents an alert post a user can make that will display on live feed */
     private PostType postType;
     private Location location;
-    private HashSet<Categorization> categorizations;
+    private HashSet<Categorization> categorizations = new HashSet<>();
     private User user;
     private Timestamp date;
 
