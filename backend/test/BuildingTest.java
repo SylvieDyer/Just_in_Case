caaf1ab853e6bcdc9ed1311f0616cde02b43ec18
Cly0211 <114455146+Cly0211@users.noreply.github.com>
2022-10-20 22:34:40 -0400

    Create BuildingTest.java

---
+++ backend/test/BuildingTest.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+public class BuildingTest{
+    @Test
+    public void testInitialization(){
+        List<String> facilities = new ArrayList<String>();
+        facilities.add("restroom");
+        facilities.add("classroom");
+        facilities.add("cafe");
+        String name = "Building A";
+        String description = "This is a building";
+        Just_in_Case.Building building = new Just_in_Case.Building(name, description, facilities);
+        assertEquals("This is a building", building.getDescription());
+        assertEquals("NOT_BUSY",building.getStatus("restroom").toString());
+        assertEquals("NOT_BUSY",building.getStatus("classroom").toString());
+        assertEquals("NOT_BUSY",building.getStatus("cafe").toString());
+    }
+
+    @Test
+    public void testAddandRemove() {
+        List<String> facilities = new ArrayList<String>();
+        facilities.add("restroom");
+        facilities.add("classroom");
+        String name = "Building A";
+        String description = "This is a building";
+        Just_in_Case.Building building = new Just_in_Case.Building(name, description, facilities);
+        building.addBuildingUnit("cafe");
+        building.removeBuildingUnit("classroom");
+        assertEquals("NOT_BUSY", building.getStatus("cafe").toString());
+        assertNull(building.getStatus("classroom"));
+    }
+}
