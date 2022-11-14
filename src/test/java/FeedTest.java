6179de586add087a5bee7e2e00fc0e2bc66e51a9
alp133 <102556053+alp133@users.noreply.github.com>
2022-11-14 09:01:45 -0500

    Added Feed tests

--- src/test/java/FeedTest.java
+++ src/test/java/FeedTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 
 import com.csds393.LiveAlertPost;
 import com.csds393.Location;
+import com.csds393.Categorization;
 import com.csds393.Feed;
 import com.csds393.PostType;
 import com.csds393.User;
@@ -41,13 +42,53 @@ public class FeedTest {
     }
 
     @Test
-    public void testAddBuilding() {
+    public void testAddPosts() {
         Feed liveAlertFeed = new Feed();
         PostType liveAlertPostType = PostType.EXCESSIVE_RAIN;
         Location location = Location.EUCLID_ADELBERT;
         User user = new User("User", "Parthana", "pjg83");
         LiveAlertPost liveAlertPost = new LiveAlertPost(liveAlertPostType, location, user);
         assertTrue(liveAlertFeed.addPost(liveAlertPost)); 
+        PostType liveAlertPostType2 = PostType.EXCESSIVE_RAIN;
+        Location location2 = Location.CUTTER;
+        User user2 = new User("User", "Ana", "alp133");
+        LiveAlertPost liveAlertPost2 = new LiveAlertPost(liveAlertPostType2, location2, user2);
+        assertTrue(liveAlertFeed.addPost(liveAlertPost2)); 
+        Feed liveAlertFeed2 = new Feed();
     }
+    @Test
+    public void testFiltering() {
+        Feed liveAlertFeed = new Feed();
+        PostType liveAlertPostType = PostType.EXCESSIVE_RAIN;
+        Location location = Location.EUCLID_ADELBERT;
+        User user = new User("User", "Parthana", "pjg83");
+        LiveAlertPost liveAlertPost = new LiveAlertPost(liveAlertPostType, location, user);
+        assertTrue(liveAlertFeed.addPost(liveAlertPost)); 
+        PostType liveAlertPostType2 = PostType.EXCESSIVE_RAIN;
+        Location location2 = Location.CUTTER;
+        User user2 = new User("User", "Ana", "alp133");
+        LiveAlertPost liveAlertPost2 = new LiveAlertPost(liveAlertPostType2, location2, user2);
+        assertTrue(liveAlertFeed.addPost(liveAlertPost2)); 
+        Feed liveAlertFeed2 = new Feed();
+        List<Categorization> category = new LinkedList<>();
+        category.add(Categorization.SAFETY);
+        liveAlertFeed2.filterFeed(category);
+    }
+    @Test
+    public void testGetFeed() {
+        Feed liveAlertFeed = new Feed();
+        PostType liveAlertPostType = PostType.EXCESSIVE_RAIN;
+        Location location = Location.EUCLID_ADELBERT;
+        User user = new User("User", "Parthana", "pjg83");
+        LiveAlertPost liveAlertPost = new LiveAlertPost(liveAlertPostType, location, user);
+        assertTrue(liveAlertFeed.addPost(liveAlertPost));
+        ArrayList<LiveAlertPost> testFeed = new ArrayList<>(); 
+        testFeed.add(liveAlertPost);
+        assertEquals(testFeed, liveAlertFeed.getFeed());
+    }
+
+
+
+
 }
 
