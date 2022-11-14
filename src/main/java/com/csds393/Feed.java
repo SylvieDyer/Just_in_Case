56f54b1b404f0f70ecbb39c43caa3a576bb19429
Prarthana G <prarthanajg@live.com>
2022-11-14 00:53:05 -0500

    Feed successfully integrated with database

--- src/main/java/com/csds393/Feed.java
+++ src/main/java/com/csds393/Feed.java
@@ -1,16 +1,55 @@
 package com.csds393;
 import java.util.*;
 
+import com.csds393.PostType;
+
+import java.io.*;
+import java.sql.*;
+
 /* class that stores all posts */
 public class Feed {
 
     private List<LiveAlertPost> alertFeed; //stores posts
     private List<Categorization> category; //stores categories to filter posts
+    private Connection conn;
 
     public Feed(){
         category = new ArrayList<Categorization>();
         category.add(Categorization.LIVE_ALERT);
         alertFeed = new ArrayList<LiveAlertPost>();
+
+        try {
+            openConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM just_in_case.livealertpost");
+            clearUsersFromDatabase();
+            User user = new User("User", "User", "user");
+
+            while(rs.next()) {
+                String postTypeString = rs.getString("postType");
+                PostType postType = PostType.DEFAULT;
+                for(PostType pt : PostType.values()) {
+                    if (postTypeString == pt.toString()) {
+                        postType = pt;
+                    }
+                }
+                
+                String locationString = rs.getString("location");
+                Location location = Location.DEFAULT;
+                for (Location l : Location.values()) {
+                    if(locationString == l.toString()) {
+                        location = l;
+                    }
+                }
+                System.out.println(location.toString());
+                alertFeed.add(new LiveAlertPost(postType, location, user));
+            }
+
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     // public ArrayList<LiveAlertPost> parseFeed() {
@@ -46,6 +85,28 @@ public class Feed {
         return posts;
     }
 
+    private void openConnection() throws FileNotFoundException {
+        String DB_URL = "jdbc:mysql://just-in-case.cn0mcjwf4mxn.us-east-1.rds.amazonaws.com:3306";
+        String USER = "admin";
+        Scanner fr = new Scanner(new File("././untracked.txt"));
+        String PASS = fr.nextLine();
 
+        try {
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            conn.setAutoCommit(true);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clearUsersFromDatabase() {
+        Statement stmt;
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate("DELETE FROM just_in_case.user");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/com/csds393/LiveAlertPost.java
+++ src/main/java/com/csds393/LiveAlertPost.java
@@ -23,19 +23,20 @@ public class LiveAlertPost {
 
         try {
             openConnection();
-            try(Statement stmt = conn.createStatement()) {
-                stmt.executeUpdate(
-                    "INSERT INTO just_in_case.livealertpost(postType, location, time, numUpvotes, numDownvotes)"
-                    + " VALUES ('" + postType + "', '" + location + "', '" + date + "', '" + 0 + "', '" + 0 + "')"); 
-                // stmt.executeUpdate(
-                //     "INSERT INTO just_in_case.posts"
-                //     + " VALUES ('" +  + "', '" + user.getCaseID()); 
-            } catch(SQLException e) {
-                e.printStackTrace();
-            }
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(
+                "INSERT INTO just_in_case.livealertpost(postType, location, time, numUpvotes, numDownvotes)"
+                + " VALUES ('" + postType + "', '" + location + "', '" + date + "', '" + 0 + "', '" + 0 + "')"); 
+            // stmt.executeUpdate(
+            //     "INSERT INTO just_in_case.posts"
+            //     + " VALUES ('" +  + "', '" + user.getCaseID()); 
+            
+            conn.close();
         } catch (FileNotFoundException e1) {
             e1.printStackTrace();
-        }        
+        }   catch(SQLException e) {
+            e.printStackTrace();
+        }      
     }
 
     private void openConnection() throws FileNotFoundException {
