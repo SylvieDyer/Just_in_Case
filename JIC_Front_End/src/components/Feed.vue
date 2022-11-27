5ab9ce6f24b52012bf95a1e766fe1f7307705ea5
DyerSylvie <sylvierui1@gmail.com>
2022-11-27 14:03:11 -0500

    feed shows up with latest first with a date

--- JIC_Front_End/src/components/Feed.vue
+++ JIC_Front_End/src/components/Feed.vue
@@ -2,12 +2,14 @@
     <div class="postFeed" id="postFeed">
       <ul style="list-style: none;">
         <li class="samplePost"
-          v-for="(post, index) in feed"
-          :key="index" 
+          v-for="post in feed.slice().reverse()"
+          :key="post.date" 
         >
           {{ post.postID }} <br>
           New Alert! <br>
           Reports of <b>{{ post.postType }}</b> at <b>{{ post.location }}</b> <br>
+          <br>
+          <p style="text-align: right">posted at: {{parseDate(post.date)}}</p>
         </li>
       </ul>
     </div>
@@ -45,6 +47,15 @@ export default {
       this.currentTutorial = null;
       this.currentIndex = -1;
     },
+
+    parseDate: function(date){
+     console.log(typeof(date));
+      let y = date.substring(0, 4);
+      let m = date.substring(5, 7);
+      let d = date.substring(8, 10);
+      let t = date.substring(11, 16);
+      return m + '/'+ d + '/' + y + ' @' + t;
+    }
     
   },
   mounted() {
